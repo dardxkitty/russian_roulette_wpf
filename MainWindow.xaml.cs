e2b7daae4a2586cd5d4fc28f814e0ef8d5f5b5e8
Ivan <122418485+dardxkitty@users.noreply.github.com>
2024-10-19 01:44:49 +0600

    MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,9 +15,7 @@ using System.Windows.Shapes;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
         private int count = 0;
